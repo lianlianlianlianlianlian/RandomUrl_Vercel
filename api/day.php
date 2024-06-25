e43a7fef7afd842880214519408a602c7c3e7e3a
lianlianlianlianlianlian <153711411@qq.com>
2024-06-25 16:58:11 +0800

    update

--- api/day.php
+++ api/day.php
@@ -8,30 +8,15 @@ header('Cache-Control: max-age=' . $expires);
 // 获取当前日期
 $currentDate = date('Y-m-d');
 
-// 检查是否需要更新图片
-if ($lastUpdated != $currentDate) {
-    // 读取 random.txt 文件中的 URL 列表
-    $urls = file('/var/task/user/url/random.txt', FILE_IGNORE_NEW_LINES);
+// 读取 random.txt 文件中的 URL 列表
+$urls = file('/var/task/user/url/random.txt', FILE_IGNORE_NEW_LINES);
 
-    // 获取当前跳转的 URL
-    $currentUrl = $urls[0];
+// 获取当前跳转的 URL
+$currentUrl = $urls[array_rand($urls)];
 
-    // 更新图片操作...
+// 更新图片操作...
 
-    // 将当前日期和URL写入url/day_last_updated.txt文件
-    file_put_contents('/var/task/user/url/day_last_updated.txt', $currentDate . ' ' . $currentUrl);
-
-    // 跳转到当前 URL
-    header('Location: ' . $currentUrl);
-    exit();
-} else {
-    // 使用上次跳转的 URL 进行操作...
-    // 读取url/day_last_updated.txt文件
-    $lastUpdatedData = file_get_contents('/var/task/user/url/day_last_updated.txt');
-    // 分割日期和URL
-    list($lastUpdatedDate, $lastUrl) = explode(' ', $lastUpdatedData);
-    // 跳转到上次 URL
-    header('Location: ' . $lastUrl);
-    exit();
-}
+// 跳转到当前 URL
+header('Location: ' . $currentUrl);
+exit();
 ?>
